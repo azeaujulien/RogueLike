695ebe76aa3de213779147ac6569a4469f939115
julien <azeau_j>
2020-11-05 16:54:32 +0100

    Clean code of inventory

--- Assets/Scripts/Player/Inventory/UI/ItemHolder.cs
+++ Assets/Scripts/Player/Inventory/UI/ItemHolder.cs
@@ -11,6 +11,9 @@ public class ItemHolder : MonoBehaviour
     public TextMeshProUGUI amountText;
     [SerializeField] private Item item;
 
+    /// <summary>
+    ///     This function init all UI info of item
+    /// </summary>
     public void InitGraphics()
     {
         image.sprite = item.itemImage;
@@ -23,17 +26,26 @@ public class ItemHolder : MonoBehaviour
         }
     }
 
+    /// <summary>
+    ///     This function update the text of the number of item in inventory
+    /// </summary>
     public void UpdateAmountGraphics()
     {
         amountText.text = item.GetNumberInInventory().ToString();
     }
     
+    /// <summary>
+    ///     This function change item in holder and reinit the UI
+    /// </summary>
     public void ChangeItem(Item newItem)
     {
         item = newItem;
         InitGraphics();
     }
 
+    /// <summary>
+    ///     This function run the action of the item
+    /// </summary>
     public void UseItem()
     {
         item.Action();
